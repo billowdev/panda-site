55b2bbe3c8b2aaf210229e6589cbc82b51baa012
lacakp <lacakp.contact@gmail.com>
2021-10-24 10:32:18 +0700

    initial

---
+++ components/footer.vue
@@ -0,0 +1,109 @@
+<template>
+  <footer class="site-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h6>About</h6>
+          <p>
+            สวัสดีครับ ผมชื่อแล็คนะครับ (lacakkarapon) ปัจจุบันผมเป็นนักศึกษา
+            วิทยาการคอมพิวเตอร์ <br />
+            ที่ชื่นชอบในการเขียนโปรแกมด้วยภาษา Python ซึ่งเป็น universal
+            programming language ใช้เขียนได้หลากหลายงาน เช่น การใช้เทคนิค Web
+            Scraping และ การทำ Machine Learning หรืองานด้าน Data Scince ครับ
+          </p>
+        </div>
+        <div class="col-md-3">
+          <h6>Categories</h6>
+          <ul class="footer-links">
+            <li>
+              <a
+                href="https://www.youtube.com/watch?v=d7oxMSDaAXE"
+                target="_blank"
+              >
+                Basic Login php + mysql Preview
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/watch?v=P9_TxAYZ20E&t=1004s"
+                target="_blank"
+              >
+                Ubuntu Server + apache2
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/watch?v=GTXUf6WDhX0"
+                target="_blank"
+              >
+                ทดลองติดตั้ง Windows 10 ใน VMware Workstation Pro 16
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/watch?v=6HxQK5mrkf8&t=178s"
+                target="_blank"
+              >
+                การลง Visual Studio 2019 และ สร้างโปรเจค คร่าวๆ
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h6>Quick Links</h6>
+          <ul class="footer-links">
+            <li>
+              <a href="https://web.facebook.com/sanedeveloper" target="_blank">
+                SaneDev's Blog
+              </a>
+            </li>
+            <li>
+              <a href="https://web.facebook.com/sanedeveloper" target="_blank">
+                สมัครสมาชิก
+              </a>
+            </li>
+            <li>
+              <a href="https://web.facebook.com/sanedeveloper" target="_blank">
+                เข้าสู่ระบบ
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+    </div>
+    <div class="container">
+      <div class="row text-center text-md-start">
+        <div class="col-12 col-md-8">
+          <p>
+            Copyright &copy; 2021 All Rights Reserved by
+            <a href="https://www.github.com/lacakp" target="_blank"> lacakp </a>
+          </p>
+        </div>
+        <div class="col-12 col-md-4">
+          <ul class="social-icons text-center text-md-end">
+            <li>
+              <a href="https://github.com/lacakp/" class="github"
+                ><i class="fab fa-github"></i
+              ></a>
+            </li>
+            <li>
+              <a
+                href="https://www.youtube.com/channel/UCGl7Rmg-RIAuxnBnBweJ9Ew"
+                class="youtube"
+                ><i class="fab fa-youtube"></i
+              ></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+ 
+<script>
+module.exports = {};
+</script>
+ 
+<style>
+</style>
---
+++ components/navbar.vue
@@ -0,0 +1,124 @@
+<template>
+  <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-transaparent">
+    <div class="container">
+      <a class="navbar-brand" href="./">
+        <img src="assets/images/T_CTP2_LOGO.png" height="50px" alt="" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarContent"
+        aria-controls="navbarContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="navbar-nav ms-auto float-end ul-navbar">
+          <li class="nav-item px-3 px-md-0 active">
+            <a class="nav-link" href="./">codetopanda's blog</a>
+          </li>
+          <li class="nav-item px-3 px-md-0" v-if="!auth.data">
+            <a class="nav-link" href="register.php">สมัครสมาชิก</a>
+          </li>
+          <li class="nav-item px-3 px-md-0" v-if="!auth.data">
+            <a class="nav-link" href="login.php">เข้าสู่ระบบ</a>
+          </li>
+
+          <li class="nav-item dropdown" v-if="auth.data">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navebarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <img
+                class="profile-img"
+                src="assets/images/uploads/avatar.png"
+                width="40px"
+                height="40px"
+                alt="avatar"
+              />
+              FirstName
+            </a>
+            <ul
+              class="dropdown-menu dropdown-menu-right mb-3"
+              aria-labelledby="navbarDropdown"
+            >
+              <li>
+                <a class="dropdown-item" href="profile.html">
+                  <div class="d-flex align-items-center">
+                    <img
+                      class="profile-img"
+                      src="assets/images/uploads/avatar.png"
+                      width="40px"
+                      height="40px"
+                      alt="avatar"
+                    />
+                    <div class="p-3 lh-sm">
+                      <span class="fw-bold">FirstName LastName</span><br />
+                      <small>ดูโปรไฟล์ของคุณ</small>
+                    </div>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="" @click="logout">ออกจากระบบ</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="progressContainer">
+      <div class="progressBar"></div>
+    </div>
+  </nav>
+</template>
+
+<script>
+module.exports = {
+  props: ["auth"],
+  mounted() {
+    window.addEventListener("scroll", function () {
+      let wintop = this.scrollY;
+      let navbar = document.querySelector(".navbar");
+
+      let winheight = this.innerHeight;
+      let docheight = document.querySelector("body").offsetHeight;
+      let totalScroll = (wintop / (docheight - winheight)) * 100;
+
+      let bar = document.querySelector(".progressBar");
+      bar.style.width = `${totalScroll}%`;
+
+      // navbar-light bg-light
+      // navbar-dark bg-transaparent
+      if (wintop > 150) {
+        navbar.classList.remove("navbar-dark", "bg-transaparent");
+        navbar.classList.add("navbar-light", "bg-light");
+      } else {
+        navbar.classList.remove("navbar-light", "bg-light");
+        navbar.classList.add("navbar-dark", "bg-transaparent");
+      }
+    });
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      location.assign("./");
+    },
+  },
+};
+
+/**
+ * mounted() ในเหตุการณ์นี้ จะเกิดขึ้นเมื่อ DOM ถูกสร้างเสร็จแล้ว เราสามารถเข้าถึง Element ต่างๆ ใน DOM ได้ทั้งหมด
+ */
+</script>
+
+<style scoped></style>
